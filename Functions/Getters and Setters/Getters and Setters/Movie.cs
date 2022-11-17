a54cef564dc7f8328fa638cae0540826270b5396
Caleb Obaigwa <obaicaleb@gmail.com>
2022-11-17 13:31:08 +0300

    setters

---
+++ Functions/Getters and Setters/Getters and Setters/Movie.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getters_and_Setters
+{
+    class Movie
+    {
+        public string tilte;
+        public string director;
+        private string rating;
+
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            tilte = aTitle;
+            director = aDirector;
+            Rating = aRating;
+        }
+
+        public string Rating
+        {
+            get { return rating; }
+            set { 
+                if (value =="G" || value == "PG-13" || value == "PG" || value == "R" || value == "NR")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR";
+                }
+            }
+        }
+    }
+}
---
+++ Functions/Getters and Setters/Getters and Setters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getters_and_Setters
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Movie avengers = new Movie("The Avengers", "Joss Whedon", "PG");
+            Movie shrek = new Movie("Shrek", "Adae Adamson", "PG-14");
+            //G, PG, PG-13, NR
+
+            Console.WriteLine(avengers.Rating);
+            Console.ReadLine();
+        }
+    }
+}
